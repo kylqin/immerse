0d4eccf10ad210cc5bfe10fd6cffaa8ab669e631
kylqin <kylqin@outlook.com>
2020-10-25 14:05:26 +0800

    Use MatSnackModule in message service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LibraryModule } from './library/library.module';
@@ -16,7 +17,8 @@ import { ReaderModule } from './reader/reader.module';
     BrowserAnimationsModule,
     AppRoutingModule,
     LibraryModule,
-    ReaderModule
+    ReaderModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,13 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ChangeDetectorRef, NgZone } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
 
-  constructor() { }
+  constructor(private snackBar: MatSnackBar, private ngZone: NgZone) { }
 
   message(msg: string) {
     console.log('MSG::', msg);
+    this.ngZone.run(() => {
+      this.snackBar.open(msg, '', {
+        duration: 500,
+        horizontalPosition: 'right',
+        verticalPosition: 'bottom'
+      });
+    });
   }
 }
